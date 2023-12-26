5ab295aebb9fd88c0a16211f5a08f4e14bba307b
Eren Gündüz <77483696+ErenGunduzz@users.noreply.github.com>
2023-12-26 10:32:43 +0300

    Add files via upload

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+public class Program
+{
+    [DllImport("Calc.dll")]
+    public static extern int Multiply(int n1, int n2);
+
+    public static void Main()
+    {
+        Console.Write("Num1: ");
+        var num1 = Console.ReadLine();
+
+        Console.Write("\nNum2: ");
+        var num2 = Console.ReadLine();
+
+        int result = Multiply(int.Parse(num1), int.Parse(num2));
+
+        Console.WriteLine($"Result after calling C++, multiplying {num1} and {num2} is {result}");
+    }
+}
